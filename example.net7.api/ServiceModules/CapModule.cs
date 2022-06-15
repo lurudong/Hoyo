09268baa17b80ada4ef1daa297d0b5dcb2c149dd
杜宇 <dygood@outlook.com>
2022-06-15 15:48:58 +0800

    当消费者服务未注册,或者程序挂掉后.未处理数据.再次启动消费者后,无法消费之前的消息,从rabbitmq-ui中看也不存在消息,难不成用CAP要保证程序不会挂掉,程序员也不能写错代码?

--- example.net7.api/ServiceModules/CapModule.cs
+++ example.net7.api/ServiceModules/CapModule.cs
@@ -11,17 +11,17 @@ public class CapModule : AppModule
             c.DefaultGroupName = "hoyo.cap";
             _ = c.UseMongoDB(s =>
             {
-                s.DatabaseConnection = "mongodb://bl:a123456@altzyxy.com:27010/?authSource=admin&serverSelectionTimeoutMS=1000";
+                s.DatabaseConnection = "mongodb://a:123456@127.0.0.1:27017/?authSource=admin&serverSelectionTimeoutMS=1000";
                 s.DatabaseName = "hoyocap";
                 s.PublishedCollection = "hoyo.cap.published";
                 s.ReceivedCollection = "hoyo.cap.received";
             });
             _ = c.UseRabbitMQ(s =>
             {
-                s.HostName = "101.34.26.221";
-                s.Port = 40013;
+                s.HostName = "192.168.2.35";
+                s.Port = 5672;
                 s.UserName = "admin";
-                s.Password = "&duyu789";
+                s.Password = "&789";
                 s.ExchangeName = "hoyo.default.cap";
                 s.VirtualHost = "/";
             });
