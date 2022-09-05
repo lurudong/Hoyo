746f8d3811c688863c5f8780e150bbd48bb3b773
Joes <dygood@outlook.com>
2022-09-05 19:40:15 +0800

    提交一下.

--- CustomEvent/CreateOrderEvent.cs
+++ CustomEvent/CreateOrderEvent.cs
@@ -4,7 +4,7 @@ using Hoyo.EventBus.RabbitMQ.Enums;
 
 namespace CustomEvent;
 [RabbitMQ("hoyo.test", EExchange.Publish, "", "orderqueue3")]
-//[RabbitMQ("hoyo.test", EExchange.Publish, "", "orderqueue2")]
+[RabbitMQ("hoyo.test", EExchange.Publish, "", "orderqueue2")]
 public class CreateOrderEvent : IntegrationEvent
 {
     public string Message { get; set; } = default!;
--- example.net7.api/Controllers/TestEventBusController.cs
+++ example.net7.api/Controllers/TestEventBusController.cs
@@ -22,25 +22,25 @@ public class TestEventBusController : ControllerBase
         _integrationEventBus.Publish<CreateOrderEvent>(orderEvent);
     }
 
-    [HttpGet]
-    public void CreateTestEvent()
-    {
-        var test = new TestEvent() { Message = "大黄瓜1CM，真的猛" };
-        _integrationEventBus.Publish<TestEvent>(test);
-    }
-    [HttpPost("TTLTest")]
-    public void TTLTest()
-    {
-        var rand = new Random();
-        var ttl = rand.Next(1000, 10000);
-        var ttlobj = new DelayedMessageEvent() { Message = $"延迟{ttl}毫秒,当前时间{DateTime.Now:yyyy-MM-dd HH:mm:ss},猛不起来了" };
-        _integrationEventBus.PublishWithTTL<DelayedMessageEvent>(ttlobj, (uint)ttl);
-    }
+    //[HttpGet]
+    //public void CreateTestEvent()
+    //{
+    //    var test = new TestEvent() { Message = "大黄瓜1CM，真的猛" };
+    //    _integrationEventBus.Publish<TestEvent>(test);
+    //}
+    //[HttpPost("TTLTest")]
+    //public void TTLTest()
+    //{
+    //    var rand = new Random();
+    //    var ttl = rand.Next(1000, 10000);
+    //    var ttlobj = new DelayedMessageEvent() { Message = $"延迟{ttl}毫秒,当前时间{DateTime.Now:yyyy-MM-dd HH:mm:ss},猛不起来了" };
+    //    _integrationEventBus.PublishWithTTL<DelayedMessageEvent>(ttlobj, (uint)ttl);
+    //}
 
-    [HttpPost("TTLTest_1")]
-    public void TTLTest_1()
-    {
-        var ttlobj = new DelayedMessageEvent2() { Message = $"大黄瓜0.5cm,当前时间{DateTime.Now:yyyy-MM-dd HH:mm:ss},猛不起来了" };
-        _integrationEventBus.Publish<DelayedMessageEvent2>(ttlobj);
-    }
+    //[HttpPost("TTLTest_1")]
+    //public void TTLTest_1()
+    //{
+    //    var ttlobj = new DelayedMessageEvent2() { Message = $"大黄瓜0.5cm,当前时间{DateTime.Now:yyyy-MM-dd HH:mm:ss},猛不起来了" };
+    //    _integrationEventBus.Publish<DelayedMessageEvent2>(ttlobj);
+    //}
 }
